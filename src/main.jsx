7ad6464849469764223d111fe8e89c232447242e
Dilan Tharaka <98627647+dilantharaka22759@users.noreply.github.com>
2025-12-01 16:07:21 +0530

    Initial commit for dv_tours_new

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // <-- import HashRouter
+import { HashRouter } from 'react-router-dom'; // Use HashRouter
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
